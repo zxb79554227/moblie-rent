e0f1db1a8da476c98c0af8f97df56fee5e254900
Hanfeng <hanfengwebdesign@gmail.com>
2020-06-01 11:43:00 +0800

    1

---
+++ src/views/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    123
+      <!-- <Login v-if="!userLogin"></Login>
+      <Home v-else></Home> -->
+  </div>
+</template>
+
+<script>
+
+export default {
+  data () {
+    return {
+      userLogin: false
+    }
+  }
+  // components: {
+  //   Login,
+  //   Home
+  // }
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
