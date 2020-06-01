2e399c5100891f8ea20f4f2db18a0f276090b85a
Hanfeng <hanfengwebdesign@gmail.com>
2020-06-01 20:51:50 +0800

    1

---
+++ vue.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    publicPath:"./",
+    outputDir:"dist",
+    assetsDir:"assets",
+    indexPath:"index.html",
+    filenameHashing:true,
+    pages:undefined,
+    lintOnSave:true,
+    runtimeCompiler:false,
+    transpileDependencies:[],
+    productionSourceMap:false,
+    crossorigin:undefined,
+    integrity:false
+}
